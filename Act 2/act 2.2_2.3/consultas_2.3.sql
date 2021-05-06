04ef2d46dc106c621eeff1393e87377485f122a4
Mathias Tejeda <mathi.tejeda@gmail.com>
2021-05-05 21:49:13 -0300

    Finally Consultas 2.3 terminadas

--- Act 2/act 2.2_2.3/consultas_2.3.sql
+++ Act 2/act 2.2_2.3/consultas_2.3.sql
@@ -101,7 +101,7 @@ GROUP by Modulos.ID,Modulos.Nombre, TiposTarea.Nombre
 --14 Por cada módulo, indicar su ID, apellido y nombre del colaborador y total que se le debe abonar en concepto de colaboraciones realizadas en dicho módulo.
 
 SELECT
-    Modulos.ID,modulos.Nombre, Colaboradores.Apellido, Colaboradores.Nombre, sum(colaboraciones.tiempo * Colaboraciones.PrecioHora) as 'honorarios'
+    Modulos.ID, modulos.Nombre, Colaboradores.Apellido, Colaboradores.Nombre, sum(colaboraciones.tiempo * Colaboraciones.PrecioHora) as 'honorarios'
 from modulos
     inner join tareas on Modulos.ID = tareas.IDModulo
     inner join Colaboraciones on tareas.ID = Colaboraciones.IDTarea
@@ -119,21 +119,102 @@ GROUP by Proyectos.Nombre
 
 --16 Listar los nombres de los proyectos que hayan registrado menos de cinco colaboradores distintos y más de 100 horas total de trabajo.
 
-
+select Proyectos.Nombre, COUNT(Colaboraciones.IDColaborador) as ' cant colaboradores', sum(Colaboraciones.Tiempo) as 'Horas de trabajo'
+from Proyectos
+    inner join Modulos on Modulos.IDProyecto = Proyectos.ID
+    inner join tareas on tareas.IDModulo = Modulos.ID
+    inner join Colaboraciones on Colaboraciones.IDTarea = Tareas.ID
+GROUP by Proyectos.Nombre
+having count(Colaboraciones.IDColaborador) < 5 and sum(Colaboraciones.Tiempo) > 100
 
 --17 Listar los nombres de los proyectos que hayan comenzado en el año 2020 que hayan registrado más de tres módulos.
---18 Listar para cada colaborador externo, el apellido y nombres y el tiempo
---máximo de horas que ha trabajo en una colaboración.
---19 Listar para cada colaborador interno, el apellido y nombres y el promedio
---percibido en concepto de colaboraciones.
---20 Listar el promedio percibido en concepto de colaboraciones para
---colaboradores internos y el promedio percibido en concepto de
+
+select Proyectos.Nombre
+from Proyectos
+    inner join Modulos on Modulos.IDProyecto = Proyectos.ID
+where Proyectos.FechaInicio >= '1-1-2020' and Proyectos.FechaInicio < '1-1-2021'
+group by Proyectos.Nombre
+having count(Modulos.ID) < 3
+
+--18 Listar para cada colaborador externo, el apellido y nombres y el tiempo máximo de horas que ha trabajo en una colaboración.
+
+select
+    Colaboradores.Apellido,
+    colaboradores.nombre,
+    max(Colaboraciones.Tiempo) as 'Maximo horas'
+from Colaboradores
+    inner join Colaboraciones on Colaboraciones.IDColaborador = Colaboradores.ID
+where Colaboradores.Tipo like 'e'
+GROUP by Colaboradores.Apellido, Colaboradores.Nombre
+
+--19 Listar para cada colaborador interno, el apellido y nombres y el promedio percibido en concepto de colaboraciones.
+
+select
+    Colaboradores.Apellido,
+    Colaboradores.Nombre,
+    avg(Colaboraciones.Tiempo * Colaboraciones.PrecioHora)
+from Colaboradores
+    inner JOIN Colaboraciones on Colaboraciones.IDColaborador = Colaboradores.ID
+where Colaboradores.Tipo like 'I'
+GROUP by Colaboradores.Nombre,Colaboradores.Apellido
+
+--20 Listar el promedio percibido en concepto de colaboraciones para colaboradores internos y el promedio percibido en concepto de
 --colaboraciones para colaboradores externos.
---21 Listar el nombre del proyecto y el total neto estimado. Este último valor surge
---del costo estimado menos los pagos que requiera hacer en concepto de
---colaboraciones.22 Listar la cantidad de colaboradores distintos que hayan colaborado en alguna
---tarea que correspondan a proyectos de clientes de tipo 'Unicornio'.
+select
+
+    avg(Colaboraciones.Tiempo * Colaboraciones.PrecioHora)
+from Colaboradores
+    inner JOIN Colaboraciones on Colaboraciones.IDColaborador = Colaboradores.ID
+where Colaboradores.Tipo like 'I'
+select
+
+    avg(Colaboraciones.Tiempo * Colaboraciones.PrecioHora)
+from Colaboradores
+    inner JOIN Colaboraciones on Colaboraciones.IDColaborador = Colaboradores.ID
+where Colaboradores.Tipo like 'E'
+
+--21 Listar el nombre del proyecto y el total neto estimado. Este último valor surge del costo estimado menos los pagos que requiera hacer 
+--en concepto de colaboraciones.
+
+select
+    Proyectos.Nombre,
+    Proyectos.CostoEstimado-sum(Colaboraciones.PrecioHora * Colaboraciones.Tiempo)
+from Proyectos
+    inner join modulos on Modulos.IDProyecto = Proyectos.ID
+    inner join tareas on Tareas.IDModulo = Modulos.ID
+    inner join Colaboraciones on Colaboraciones.IDTarea = Tareas.ID
+GROUP by Proyectos.Nombre,Proyectos.CostoEstimado
+--(I DECLARE BANKRUPCYYYYYY)
+
+--22 Listar la cantidad de colaboradores distintos que hayan colaborado en alguna tarea que correspondan a proyectos de clientes de tipo 'Unicornio'.
+
+select
+    count(Colaboraciones.IDColaborador) as 'Cantidad colabradores'
+from Proyectos
+    inner join modulos on Modulos.IDProyecto = Proyectos.ID
+    inner join Tareas on Tareas.IDModulo = Tareas.ID
+    inner join Colaboraciones on Colaboraciones.IDTarea = Tareas.ID
+    inner join Clientes on Proyectos.IDCliente = Clientes.ID
+where Clientes.IDTipo = 2
+
 --23 La cantidad de tareas realizadas por colaboradores del país 'Argentina'.
---24 Por cada proyecto, la cantidad de módulos que se haya estimado mal la fecha
---de fin. Es decir, que se haya finalizado antes o después que la fecha estimada.
---Indicar el nombre del proyecto y la cantidad calculada
+
+select
+    count(Colaboraciones.IDTarea) as 'tareas realizadas'
+from Colaboraciones
+    inner join colaboradores on Colaboraciones.IDColaborador = Colaboradores.ID
+    inner join Ciudades on colaboradores.IDCiudad = Ciudades.ID
+    inner join Paises on Ciudades.IDPais = Paises.ID
+where Paises.Nombre like 'argentina'
+
+
+--24 Por cada proyecto, la cantidad de módulos que se haya estimado mal la fecha de fin. Es decir, que se haya finalizado antes o 
+--después que la fecha estimada. Indicar el nombre del proyecto y la cantidad calculada
+
+select
+    Proyectos.Nombre,
+    count(Modulos.ID) as 'destiempo'
+from Proyectos
+    inner join Modulos on Proyectos.ID = Modulos.IDProyecto
+where Modulos.FechaFin > Modulos.FechaEstimadaFin or Modulos.FechaEstimadaFin > Modulos.FechaFin
+GROUP by Proyectos.Nombre
